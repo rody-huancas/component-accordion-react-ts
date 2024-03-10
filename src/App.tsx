cbacd90f020c5182ff370e5d2e439f89d96590e2
Rody Huancas <rodyhuancas.04@gmail.com>
2024-03-09 22:28:38 -0500

    Add: functionality accordion

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
-import { Accordion } from "./components/Accordion"
+import { Accordion } from "./components/Accordion";
 
 const App = () => {
+  const list = [
+    {
+      question: "Whats your name?",
+      answer: "My name is Rody Huancas",
+      active: 0,
+    },
+    {
+      question: "Whats do you?",
+      answer: "Full-stack developer and designer",
+      active: 0,
+    },
+  ];
+
   return (
     <div className="bg-[#f2f2f2] h-screen flex items-center justify-center">
       <div className="list flex flex-col gap-5">
-        <Accordion />
-        <Accordion />
+        {list.map((item, index) => {
+          return (
+            <Accordion
+              key={index}
+              question={item.question}
+              answer={item.answer}
+              active={item.active}
+            />
+          );
+        })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,16 +1,32 @@
-export const Accordion = () => {
+import React, { useState } from "react";
+
+interface PropsAccordion {
+  question: string;
+  answer: string;
+  active?: number;
+}
+
+export const Accordion: React.FC<PropsAccordion> = ({ question, answer, active = 0 }) => {
+  const [isActive, setIsActive] = useState(active === 1);
+
+  const handleToggleActive = () => setIsActive(!isActive);
+
   return (
-    <div className="bg-[#e9e9e9] p-5 border border-[#c9c6c655] rounded-md w-[280px] group is-active">
+    <div
+      className={`bg-[#e9e9e9] p-5 border border-[#c9c6c655] rounded-md w-[280px] group cursor-pointer ${
+        isActive ? "is-active bg-white" : ""
+      }`}
+      onClick={handleToggleActive}
+    >
       <div className="flex items-center">
-        <div className="w-full group-[.is-active]:font-bold">
-          Whats your name?
-        </div>
-        <div className="text-xl rotate-90 group-[.is-active]:rotate-[270deg]">
+        <div className="w-full font-medium duration-500">{question}</div>
+        <div className="text-xl rotate-90 group-[.is-active]:rotate-[270deg] duration-500">
           {">"}
         </div>
       </div>
-      <div className="overflow-hidden max-h-0 group-[.is-active]:max-h-[100px]">
-        Mi name is Rody Huancas
+      <hr className="hidden group-[.is-active]:block border border-b-gray-300 my-1" />
+      <div className="overflow-hidden max-h-0 group-[.is-active]:max-h-[100px] duration-500">
+        {answer}
       </div>
     </div>
   );
